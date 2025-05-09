3009e9dff047c2155c823bee0cc61e9f3646cfaa
JarkkoKarki <karkijarkko@gmail.com>
2025-05-09 18:32:02 +0300

    Add Chisel item with block transformation functionality

--- src/main/java/net/jk/mod/item/ModItems.java
+++ src/main/java/net/jk/mod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jk.mod.item;
 
 import net.jk.mod.MinecraftMod;
+import net.jk.mod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,7 +16,7 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite", () -> new Item(new Item.Properties()));
 
-
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/net/jk/mod/item/custom/ChiselItem.java
@@ -0,0 +1,54 @@
+package net.jk.mod.item.custom;
+
+import net.jk.mod.block.ModBlocks;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.Map;
+
+public class ChiselItem extends Item {
+    public static final Map<Block, Block> CHISEL_MAP = Map.of(
+            Blocks.STONE, Blocks.STONE_BRICKS,
+            Blocks.END_STONE, Blocks.END_STONE_BRICKS,
+            Blocks.DIORITE, Blocks.POLISHED_DIORITE,
+            Blocks.ANDESITE, Blocks.POLISHED_ANDESITE,
+            Blocks.GRANITE, Blocks.POLISHED_GRANITE,
+            Blocks.NETHERRACK, Blocks.NETHER_BRICKS,
+            Blocks.SOUL_SAND, Blocks.SOUL_SOIL,
+            Blocks.SOUL_SOIL, Blocks.SOUL_SAND,
+            Blocks.QUARTZ_BLOCK, Blocks.QUARTZ_BRICKS,
+            Blocks.DIRT, ModBlocks.ALEXANDRITE_BLOCK.get()
+    );
+
+    public ChiselItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        Level level = pContext.getLevel();
+        Block clickedBlock = level.getBlockState(pContext.getClickedPos()).getBlock();
+
+        if(CHISEL_MAP.containsKey(clickedBlock)) {
+            if(!level.isClientSide()) {
+                level.setBlockAndUpdate(pContext.getClickedPos(), CHISEL_MAP.get(clickedBlock).defaultBlockState());
+
+                pContext.getItemInHand().hurtAndBreak(1, ((ServerLevel) level), ((ServerPlayer) pContext.getPlayer()),
+                        item -> pContext.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+
+                level.playSound(null, pContext.getClickedPos(), SoundEvents.GRINDSTONE_USE, SoundSource.BLOCKS);
+            }
+        }
+
+        return InteractionResult.SUCCESS;
+    }
+}
