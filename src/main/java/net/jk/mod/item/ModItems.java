300fa8526d44549511a61c348ac2d17d8990c0cb
JarkkoKarki <karkijarkko@gmail.com>
2025-05-11 05:51:51 +0300

    Add Aurora Ashes item with custom burn time and model

--- src/main/java/net/jk/mod/item/ModItems.java
+++ src/main/java/net/jk/mod/item/ModItems.java
@@ -20,6 +20,8 @@ public class ModItems {
 
     public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi", () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI)));
 
+    public static final RegistryObject<Item> AURORA_ASHES = ITEMS.register("aurora_ashes", () -> new FuelItem(new Item.Properties(), 1200));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
