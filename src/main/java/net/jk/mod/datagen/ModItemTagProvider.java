ea5b5665421d75340e7952eb46878b681c44bbe1
JarkkoKarki <karkijarkko@gmail.com>
2025-05-12 14:55:41 +0300

    Add data generation for Alexandrite items, including tags, recipes, and loot tables

---
+++ src/main/java/net/jk/mod/datagen/ModItemTagProvider.java
@@ -0,0 +1,31 @@
+package net.jk.mod.datagen;
+
+import net.jk.mod.MinecraftMod;
+import net.jk.mod.item.ModItems;
+import net.jk.mod.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends ItemTagsProvider {
+    public ModItemTagProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> completableFuture,
+                              CompletableFuture<TagLookup<Block>> lookupCompletableFuture, @Nullable ExistingFileHelper existingFileHelper) {
+        super(packOutput, completableFuture, lookupCompletableFuture, MinecraftMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        tag(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.ALEXANDRITE.get())
+                .add(ModItems.RAW_ALEXANDRITE.get())
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.COMPASS);
+    }
+}
