96aacb3018ab6c71d921cc1964e16be4d6c97932
Jarkko Kärki <152171266+JarkkoKarki@users.noreply.github.com>
2025-05-16 13:53:24 +0300

    Events

--- src/main/java/net/jk/mod/event/ModEvents.java
+++ src/main/java/net/jk/mod/event/ModEvents.java
@@ -3,9 +3,16 @@ package net.jk.mod.event;
 import net.jk.mod.MinecraftMod;
 import net.jk.mod.item.custom.HammerItem;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -37,6 +44,17 @@ public class ModEvents {
                 HARVESTED_BLOCKS.add(pos);
                 serverPlayer.gameMode.destroyBlock(pos);
                 HARVESTED_BLOCKS.remove(pos);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLivingDamage(LivingDamageEvent event) {
+        if(event.getEntity() instanceof Sheep sheep && event.getSource().getDirectEntity() instanceof Player player) {
+            if(player.getMainHandItem().getItem() == Items.END_ROD) {
+                player.sendSystemMessage(Component.literal(player.getName().getString() + " has used the End Rod on a sheep!"));
+                sheep.addEffect(new MobEffectInstance(MobEffects.POISON, 600, 5));
+                player.getMainHandItem().shrink(1);
             }
         }
     }
